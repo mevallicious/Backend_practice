6e1e7c7ea9281acaa962d134af9185aa5f14d258
Meval Purohit <mevalpurohit2828@gmail.com>
2026-03-17 12:09:12 +0530

    perplecity initialization of socket at server and backend

--- day32_perplexity/backend/server.js
+++ day32_perplexity/backend/server.js
@@ -1,11 +1,15 @@
 import "dotenv/config"
 import app from "./src/app.js"
+import http from "http"
 import { connectToDB } from "./src/config/database.js"
-import { testAI } from "./src/services/ai.service.js"
+import { initSocket } from "./src/sockets/server.socket.js"
 
-testAI()
+const httpServer= http.createServer(app)
+
+initSocket(httpServer)
+
 connectToDB()
 
-app.listen(3000,()=>{
+httpServer.listen(3000,()=>{
     console.log("server is connected on port 3000")
 })
