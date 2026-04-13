ad935907747fda9c23dec04fb52aa65fdb5acbd4
Meval Purohit <mevalpurohit2828@gmail.com>
2026-04-13 13:18:33 +0530

    implemented react router in the frontend

--- Snitch/frontend/src/app/App.jsx
+++ Snitch/frontend/src/app/App.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { RouterProvider } from 'react-router'
+import { routes } from './app.routes'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <RouterProvider router={routes}/>
+    </div>
   )
 }
 
---
+++ Snitch/frontend/src/app/app.routes.jsx
@@ -0,0 +1,9 @@
+import { createBrowserRouter } from "react-router";
+import App from "./App";
+
+export const routes = createBrowserRouter([
+    {
+        path:"/",
+        element: <h1>Hello ji hello</h1>
+    }
+])
