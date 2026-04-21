8512d226273841aee5d0af081f8560dc75d41811
Meval Purohit <mevalpurohit2828@gmail.com>
2026-04-21 16:39:58 +0530

    added path import

--- Snitch/backend/src/app.js
+++ Snitch/backend/src/app.js
@@ -21,7 +21,10 @@ app.use(express.json())
 app.use(express.urlencoded({ extended:true }))
 app.use(cookieParser())
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin: [
+    'http://localhost:5173',                   
+    "https://urbanneeds-1tgi.onrender.com"
+  ],
     methods:["GET","POST","PUT","DELETE"],
     credentials:true
 }))
